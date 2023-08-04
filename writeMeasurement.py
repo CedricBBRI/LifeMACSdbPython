a8634f7702e1b0cf75e727154266bebf45bcf30e
cedric driesen <cedric.driesen@buildwise.be>
2023-08-04 12:09:25 +0200

    writeMeasurement init

--- writeMeasurement.py
+++ writeMeasurement.py
@@ -1,7 +1,15 @@
 import psycopg2
 from datetime import datetime
 
-# Set up the database connection
+# Predefined list of parameter names
+valid_parameter_names = [
+    "compressive strength",
+    "strain",
+    "temperature",
+    "deflection"
+]
+
+# Connect to the database
 cnx = psycopg2.connect(
     host='buildwise.digital',
     user='postgresCedric',
@@ -13,55 +21,83 @@ cnx = psycopg2.connect(
 # Create a cursor object
 cursor = cnx.cursor()
 
-# SQL query to retrieve all unique host IDs
-host_id_query = "SELECT DISTINCT Host_ID FROM Cracks"
+# SQL query to retrieve all measurement location IDs
+location_id_query = "SELECT measurements_id FROM repeated_measurements"
 
-# Execute the host ID query
-cursor.execute(host_id_query)
-
-# Fetch all the results
-host_ids = cursor.fetchall()
-
-# Print the list of host IDs
-print("Current Host_IDs:")
-for row in host_ids:
-    print(row[0])
-
-# Ask for user input for host ID
-host_id = input("Please enter one of the above Host_IDs: ")
-
-# SQL query to retrieve all unique crack IDs for a given host
-crack_id_query = "SELECT DISTINCT Crack_ID FROM Cracks WHERE Host_ID = %(host_id)s"
-
-# Execute the crack ID query
-cursor.execute(crack_id_query, {'host_id': host_id})
+# Execute the query
+cursor.execute(location_id_query)
 
 # Fetch all the results
-crack_ids = cursor.fetchall()
+location_ids = cursor.fetchall()
 
-# Print the list of crack IDs
-print("Current Crack_IDs for selected Host_ID:")
-for row in crack_ids:
+# Print the list of measurement location IDs
+print("Current measurement location IDs:")
+for row in location_ids:
     print(row[0])
 
-# Ask for user input for crack ID
-crack_id = input("Please enter one of the above Crack_IDs: ")
+# Ask for user input for measurement location ID
+location_id = input("What is the measurement location ID? (Leave empty to create a new one): ")
 
-# Ask for user input for measurements
-length = input("Please enter the length of the crack: ")
-width = input("Please enter the width of the crack: ")
-depth = input("Please enter the depth of the crack: ")
+# If measurement location ID is empty, prompt for a new entry in the 'repeated_measurements' table
+if location_id == "":
+    host_id = input("Enter host_id: ")
+    # Split the location input into a list of strings, then convert to a list of floats
+    host_location = list(map(float, input("Enter host_location (comma-separated): ").split(",")))
+    description = input("Enter description: ")
+    measurement_type = input("Enter measurement type: ")
+    measured_by = input("Enter measured by: ")
 
-# Get current time
-now = datetime.now()
+    query = """
+    INSERT INTO repeated_measurements (host_id, host_location, description, measurement_type, measured_by)
+    VALUES (%(host_id)s, ARRAY[%(host_location)s], %(description)s, %(measurement_type)s, %(measured_by)s)
+    RETURNING measurements_id;
+    """
+
+    # Execute the query
+    cursor.execute(query, {'host_id': host_id, 'host_location': host_location, 'description': description,
+                           'measurement_type': measurement_type, 'measured_by': measured_by})
+    location_id = cursor.fetchone()[0]
+    cnx.commit()
+
+elif int(location_id) not in [item[0] for item in location_ids]:
+    print("Invalid measurement location ID.")
+    cursor.close()
+    cnx.close()
+    exit()
+
+# Continue asking questions for the necessary parameters
+parameter_name = input("Enter parameter_name: ")
+measured_value = float(input("Enter measured_value: "))
+distribution_type = input("Enter distribution_type (Leave empty if not applicable): ")
+
+if distribution_type != "":
+    standard_deviation = float(input("Enter standard_deviation: "))
+else:
+    standard_deviation = None
+
+time_of_measurement = datetime.now()
+measured_by = input("Enter measured by: ")
+description = input("Enter description: ")
 
 # Print the entered information and ask for confirmation
 print(f'\nYou entered the following information:')
-print(f'Crack_ID: {crack_id}')
-print(f'Length: {length}')
-print(f'Width: {width}')
-print(f'Depth: {depth}')
-print(f'Time of Measurement: {now}')
+print(f'Measurement location ID: {location_id}')
+print(f'Parameter Name: {parameter_name}')
+# Check if the parameter name is valid
+if parameter_name not in valid_parameter_names:
+    print(f"Warning: {parameter_name} is not in the predefined list.")
+    confirm_parameter_name = input("Do you want to continue with this parameter name? [y/n]: ")
+    if confirm_parameter_name.lower() != 'y':
+        print("Operation cancelled.")
+        cursor.close()
+        cnx.close()
+        exit()
+print(f'Measured Value: {measured_value}')
+print(f'Distribution Type: {distribution_type}')
+print(f'Standard Deviation: {standard_deviation}')
+print(f'Time of Measurement: {time_of_measurement}')
+print(f'Measured by: {measured_by}')
+print(f'Description: {description}')
 
 confirm = input('\nDo you want to add this information to the database? [y/n]: ')
 if confirm.lower() != 'y':
@@ -69,16 +105,17 @@ if confirm.lower() != 'y':
 else:
     # SQL query
     query = """
-    INSERT INTO Measurements (Crack_ID, Time_of_Measurement, Length, Width, Depth)
-    VALUES (%(crack_id)s, %(time)s, %(length)s, %(width)s, %(depth)s);
+    INSERT INTO measurements (measurements_id, parameter_name, measured_value, distribution_type, standard_deviation, time_of_measurement, measured_by, description)
+    VALUES (%(location_id)s, %(parameter_name)s, %(measured_value)s, %(distribution_type)s, %(standard_deviation)s, %(time_of_measurement)s, %(measured_by)s, %(description)s);
     """
 
-    # Execute the query with parameters
-    cursor.execute(query, {'crack_id': crack_id, 'time': now, 'length': length, 'width': width, 'depth': depth})
+    # Execute the query
+    cursor.execute(query, {'location_id': location_id, 'parameter_name': parameter_name, 'measured_value': measured_value,
+                           'distribution_type': distribution_type, 'standard_deviation': standard_deviation, 'time_of_measurement': time_of_measurement,
+                           'measured_by': measured_by, 'description': description})
 
     # Commit the transaction
     cnx.commit()
-
     print("New measurement added successfully.")
 
 # Close the cursor and connection
